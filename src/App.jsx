ea5002a1a8a3a564c0f6c97a73379c5bc574ab78
ariwibi <ari.bowo.0231@gmail.com>
2023-11-29 14:32:54 +0800

    membuat app dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,71 @@
-function App() {
-  return;
-}
+// import { ThemeProvider } from './contexts/ThemeContext';
+// import { useState, useEffect } from 'react';
+// import ToggleTheme from './components/ToggleTheme';
 
+// function App() {
+//   const [theme, setTheme] = useState('light');
+//   useEffect(() => {
+//     const toggleTheme = () => {
+//       setTheme(theme === 'light' ? 'dark' : 'light');
+//     };
+//     toggleTheme();
+//   }, []);
+
+//   const themeContextValue = {
+//     theme: theme,
+//     setTheme: setTheme
+//   };
+
+//   return (
+//     <ThemeProvider value={themeContextValue}>
+//       <div className="container">
+//         <ToggleTheme />
+//         <p>Hello World</p>
+//       </div>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
+import { ThemeProvider } from './contexts/ThemeContext';
+import ToggleTheme from './components/ToggleTheme';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: localStorage.getItem('theme') || 'light',
+      toggleTheme: () => {
+        this.setState((prevState) => {
+          const newTheme = prevState.theme === 'light' ? 'dark' : 'light';
+          localStorage.setItem('theme', newTheme);
+          return {
+            theme: newTheme
+          };
+        });
+      }
+    };
+  }
+  componentDidMount() {
+    document.documentElement.setAttribute('data-theme', this.state.theme);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.theme !== this.state.theme) {
+      document.documentElement.setAttribute('data-theme', this.state.theme);
+    }
+  }
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <ToggleTheme />
+          <p>Hello, {this.state.theme === 'light' ? 'Early Bird' : 'Night Owl'}</p>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
 export default App;
